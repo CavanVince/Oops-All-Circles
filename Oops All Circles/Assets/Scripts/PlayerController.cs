8ea18f11c1ac51a67cc1080ae87543266ae14cfb
CavanVince <csvince87@gmail.com>
2021-07-05 01:40:46 +0200

    Added Basic Player Movement Controls.

---
+++ Oops All Circles/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();   
+    }
+
+    /// <summary>
+    /// Moves the player object based on the given user input.
+    /// </summary>
+    void MovePlayer()
+    {
+        transform.Translate(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f);
+    }
+}

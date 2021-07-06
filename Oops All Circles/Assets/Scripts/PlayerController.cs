17ce0b7625b1c47f7b01350240e8ba78a3fd584f
CavanVince <csvince87@gmail.com>
2021-07-07 00:03:56 +0200

    Added a simple line renderer for the aimer, and very simple bullet instantiation.

--- Oops All Circles/Assets/Scripts/PlayerController.cs
+++ Oops All Circles/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private GameObject bullet;
 
     // Start is called before the first frame update
     void Start()
     {
-
+   
     }
 
     // Update is called once per frame
     void Update()
     {
-        MovePlayer();   
+        //Move the player
+        MovePlayer();
+
+        //Render line for aiming
+        AimLine();
+
+        //Shoot the bullets
+        ShootBullet();
     }
 
     /// <summary>
@@ -24,5 +34,28 @@ public class PlayerController : MonoBehaviour
     void MovePlayer()
     {
         transform.Translate(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f);
+    }
+
+    /// <summary>
+    /// Renders the line that the player uses to aim
+    /// </summary>
+    void AimLine() 
+    {
+        Vector2 playerPosition = transform.position;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        lineRenderer.SetPosition(0, playerPosition);
+        lineRenderer.SetPosition(1, mousePosition);
+    }
+
+    /// <summary>
+    /// Instantiates a bullet prefab when the player left clicks
+    /// </summary>
+    void ShootBullet() 
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+        }
     }
 }
